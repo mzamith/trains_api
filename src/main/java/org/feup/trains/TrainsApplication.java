0cfeb7e0a3949a936c8e8dec6204673ce58e7dbe
Manuel Zamith <mzamith10@gmail.com>
2017-03-04 21:26:27 +0000

    Spring Project

---
+++ src/main/java/org/feup/trains/TrainsApplication.java
@@ -0,0 +1,12 @@
+package org.feup.trains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainsApplication.class, args);
+	}
+}
